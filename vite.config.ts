549b5d37c1a6a4072d6ba4e1bdfffc6feed7ff75
zetharionn <zetharionn@gmail.com>
2024-11-11 17:03:07 +0300

    feat: aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import path from 'node:path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@app': path.resolve(__dirname, './src/app'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@shared': path.resolve(__dirname, './src/shared')
+		}
+	}
 })
